7d956068e7f49e98ad3d5b01ab92bbd9d2beb285
Christine Elaine Lynch <lalainelynch@gmail.com>
2023-07-18 17:38:39 +0800

    added footer to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   return (
     <>
       <Navbar />
       <h1>Hello World!</h1>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer () {
+    return (
+        <>
+           <footer className="bg-info">
+               <div className="text-center mb-0 h5">© 2023 All Rights Reserved</div>
+           </footer>
+        </>
+    )
+}
+
+export default Footer
