f69924a27a6839ade37295c7ae437cdf7a7fb74f
Christine Elaine Lynch <lalainelynch@gmail.com>
2023-07-18 22:43:48 +0800

    added props, conditional  and loops to maincontainer

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,40 @@ import Footer from './components/Footer.jsx'
 import MainContainer from './components/MainContainer.jsx'
 
 function App() {
+   const expenses = [
+    {
+      id: 1,
+      grocery: '1000',
+      electricity: '2500',
+      total: 3500
+    },
+    {
+      id: 2,
+      grocery: '800',
+      electricity: '500',
+      total: 1300
+    },
+    // {
+    //   id: 3,
+    //   grocery: '500',
+    //   electricity: '2500'
+    // },
+    // {
+    //   id: 4,
+    //   grocery: '2000',
+    //   electricity: '2500'
+    // },
+   ]
+
+   const MyExpensesList = expenses.map(
+    (expense) => <MainContainer key={expense.id} grocery={expense.grocery} electricity={expense.electricity} total={expense.total}/>
+   );
+
   return (
     <>
       <Navbar />
       <MainContainer />
+      { MyExpensesList }
       <Footer />
     </>
   )
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,13 +1,28 @@
-function MainContainer () {
+function MainContainer (props) {
+    const { grocery, electricity, total } = props;
+
+    let totalExpenseEvaluation;
+    if (total > 1500) {
+        totalExpenseEvaluation = <h3>You are Broke!</h3>
+    } else {
+        totalExpenseEvaluation = <h3>You have Saved!</h3>
+    }
+
+
     return (
         <>
            <main className="bg-primary">
                <h1 className="text-center">My Expenses</h1>
                <div className="row">
                   <div className="col-sm-3">
-                     <div className="card bg-warning">
+                     <div className={`card ${total > 1500 ? 'bg-danger' : 'bg-success'}`}>
                          <div className="card-body">
                               <h5 className="card-title">Monday</h5>
+                              <p>Grocery: <span>{grocery}</span></p>
+                              <p>Electricity: <span>{electricity}</span></p>
+                              <p>Total: <span>{total}</span></p> 
+                              {totalExpenseEvaluation}
+                        
                          </div>
                      </div>
                  </div>
@@ -15,6 +30,8 @@ function MainContainer () {
                        <div className="card bg-warning">
                             <div className="card-body">
                                <h5 className="card-title">Tuesday</h5>
+                               <p>Grocery: <span>{grocery}</span></p>
+                               <p>Electricity: <span>{electricity}</span></p>
                           </div>
                      </div>
                  </div>
@@ -22,6 +39,8 @@ function MainContainer () {
                       <div className="card bg-warning">
                           <div className="card-body">
                                <h5 className="card-title">Wednesday</h5>
+                               <p>Grocery: <span>{grocery}</span></p>
+                               <p>Electricity: <span>{electricity}</span></p>
                            </div>
                        </div>
                  </div>
@@ -29,6 +48,8 @@ function MainContainer () {
                        <div className="card bg-warning">
                             <div className="card-body">
                                <h5 className="card-title">Thursday</h5>
+                               <p>Grocery: <span>{grocery}</span></p>
+                               <p>Electricity: <span>{electricity}</span></p>
                            </div>
                        </div>
                    </div>
